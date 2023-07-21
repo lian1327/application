b65af2bc106189fac4582f3c3fefd5c5ea90f3b5
leonardmesiera@outlook.com <leonardmesiera@outlook.com>
2023-07-21 19:11:27 +0800

    first commit

---
+++ application-main/README.py
@@ -0,0 +1,16 @@
+## Thank you for your interest in Python Data Expert @ BRJ Enterprises. 
+## To be considered for an interview please follow the instructions below.
+## Uncomment each step in this file once you've completed it. 
+
+
+## Step 1: 
+# Clone this repository and make it public.
+
+## Step 2: 
+# Review the Job Description in the Docs Folder. 
+
+## Step 3:
+# Build a python class in the sample_proj directory.
+
+## Step 4:
+# Publish your changes to the repository and add your public repository link to your applicaiton.
